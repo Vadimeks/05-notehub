8b1912ef717f412a4c0ac23e1b53c6c7a839b7c4
Vadzim Simanau <eks220885@gmail.com>
2025-07-21 19:04:19 +0300

    fix index.css, refresh App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,9 @@ import css from "./App.module.css";
 export default function App() {
   return (
     <div className={css.app}>
-      <h1>NoteHub</h1>
+      <header className={css.toolbar}>
+        <button className={css.button}>Create note +</button>
+      </header>
     </div>
   );
 }
